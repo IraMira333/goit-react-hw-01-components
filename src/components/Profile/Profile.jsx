f0cdcf93f4ca6d4ca840e359add85e1c3b9f843e
IraMira333 <irinat333@gmail.com>
2023-06-21 00:57:56 +0300

    Create Profile.jsx

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+export default function Profile({
+  avatar,
+  username,
+  tag,
+  location,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img
+          src={avatar}
+          alt="User avatar"
+          className={css.avatar}
+          width="150"
+          height="150"
+        />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li className={css.item}>
+          <span className={css.label}>Followers</span> <br></br>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Views</span> <br></br>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Likes</span> <br></br>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+Profile.propTypes = {
+  avatar: PropTypes.string,
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  stats: PropTypes.object,
+};
