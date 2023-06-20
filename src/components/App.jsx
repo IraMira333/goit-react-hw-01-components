6fad81243e00876fc05dd73396e44fdcd89620a4
IraMira333 <irinat333@gmail.com>
2023-06-20 14:01:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import Profile from './Profile';
 import user from '../path/user.json';
+import data from '../path/data.json';
+import friends from '../path/friends.json';
+import transactions from '../path/transactions.json';
+import Statistics from './Statistics';
+import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        display: 'block',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 24,
         color: '#010101',
       }}
     >
@@ -19,6 +25,11 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
