1c278a145828f1deeb4bccbe4960b89d4e7245fc
IraMira333 <irinat333@gmail.com>
2023-06-21 13:03:33 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -4,7 +4,7 @@ import css from './FriendList.module.css';
 
 export default function FriendList({ friends }) {
   return (
-    <ul className={css['friend-list']}>
+    <ul className={css.friend_list}>
       {friends.map(friend => (
         <FriendListItem
           key={friend.id}
@@ -19,5 +19,5 @@ export default function FriendList({ friends }) {
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({ id: PropTypes.number.isRequired })
-  ),
+  ).isRequired,
 };
